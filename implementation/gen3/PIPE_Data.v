5ef3983c4bd7c1d581022abede02e9f6724c8365
salmasaher <79278731+salmasaher@users.noreply.github.com>
2021-06-02 17:40:59 +0200

    Add files via upload

--- implementation/gen3/PIPE_Data.v
+++ implementation/gen3/PIPE_Data.v
@@ -4,7 +4,7 @@ module PIPE_Data #(parameter pipe_width_gen1 = 8,
                    parameter pipe_width_gen4 = 32,
                    parameter pipe_width_gen5 = 32
 
-  )(generation, pclk, reset_n, scramblerDataOut, scramblerDataK, scramblerSyncHeader, scramblerDataValid, TxData, TxDataValid, TxDataK, TxSyncHeader);
+  )(generation, pclk, reset_n, scramblerDataOut, scramblerDataK, scramblerSyncHeader, scramblerDataValid, TxData, TxDataValid, TxDataK, TxSyncHeader, TxStartBlock);
 
 
 //reg [5:0] pipe_width;
@@ -17,7 +17,7 @@ input [1:0] scramblerSyncHeader;
 input scramblerDataValid;
 
 output reg [31:0] TxData; 
-output reg TxDataValid; 
+output reg TxDataValid, TxStartBlock; 
 output reg [3:0] TxDataK;
 output reg [1:0] TxSyncHeader;
 
@@ -27,6 +27,7 @@ always @(posedge pclk or negedge reset_n) begin
   	TxDataK = 0;
   	TxDataValid = 0;
     TxSyncHeader = 0;
+    TxStartBlock = 0;
   end
   else if (generation==1) begin
 //  		pipe_width=pipe_width_gen1;
@@ -46,26 +47,39 @@ always @(posedge pclk or negedge reset_n) begin
   		TxDataK=scramblerDataK[(pipe_width_gen3/8)-1:0];
   		TxDataValid= scramblerDataValid;
       TxSyncHeader= scramblerSyncHeader;
+      if(scramblerSyncHeader==2'b10 || scramblerSyncHeader==2'b01)
+          TxStartBlock=1;
+      else 
+          TxStartBlock=0;
   end 
   else if (generation==4) begin
     //	pipe_width=pipe_width_gen4;
   		TxData= scramblerDataOut[pipe_width_gen4-1:0];
   		TxDataK=scramblerDataK[(pipe_width_gen4/8)-1:0];
   		TxDataValid= scramblerDataValid;
-      TxSyncHeader= scramblerSyncHeader;      
+      TxSyncHeader= scramblerSyncHeader;
+      if(scramblerSyncHeader==2'b10 || scramblerSyncHeader==2'b01)
+          TxStartBlock=1;
+      else 
+          TxStartBlock=0;      
   end
   else if (generation==5) begin
       //	pipe_width=pipe_width_gen5;
   		TxData= scramblerDataOut[pipe_width_gen5-1:0];
   		TxDataK=scramblerDataK[(pipe_width_gen5/8)-1:0];
   		TxDataValid= scramblerDataValid;
-      TxSyncHeader= scramblerSyncHeader;      
+      TxSyncHeader= scramblerSyncHeader;   
+      if(scramblerSyncHeader==2'b10 || scramblerSyncHeader==2'b01)
+          TxStartBlock=1;
+      else 
+          TxStartBlock=0;   
   end  
   else begin
   		TxData=0;
   		TxDataK=0;
   		TxDataValid=0;
       TxSyncHeader= 0;
+      TxStartBlock = 0;
   end
 end
 endmodule
