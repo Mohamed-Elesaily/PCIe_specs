c6c93adcffd90d95cf5e8356fd3149399c6064d1
Mohamed Elesaily <mohamed.elesaily98@gmail.com>
2021-07-19 02:43:26 +0200

    some changes

--- implementation/gen3/Gen_ctrl.v
+++ implementation/gen3/Gen_ctrl.v
@@ -85,7 +85,6 @@ end
 
 
 assign sel =(gen == gen1_sel | gen == gen2_sel)? 1'b0:1'b1;
-assign valid =(linkup)? valid_reg:64'b0; 
 assign w = valid_pd & linkup;
-assign valid = valid_reg;  
+assign valid =(linkup)? valid_reg:64'b0;  
 endmodule
